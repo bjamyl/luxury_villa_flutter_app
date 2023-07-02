e9a4379a0938d50ce949dd9a17040f20dddcc488
bjamyl <alhassanjamil0@gmail.com>
2023-07-02 23:36:39 +0000

    create listing detail widgets and display dynamic data from API

--- lib/screens/listing_detail_screen.dart
+++ lib/screens/listing_detail_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:luxury_villa/providers/listing.dart';
 import 'package:provider/provider.dart';
+import '../constants.dart';
 import '../providers/listings.dart';
 
 class ListingDetailScreen extends StatelessWidget {
@@ -16,53 +18,209 @@ class ListingDetailScreen extends StatelessWidget {
         .findItemById(listingId as int);
     return Scaffold(
         body: SingleChildScrollView(
-      child: Column(children: [
-        SizedBox(
-          height: deviceSize.height * 0.55,
-          child: Stack(
-            children: [
-              SizedBox(
-                height: deviceSize.height * 0.45,
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(40),
-                      bottomRight: Radius.circular(40)),
-                  child: Hero(
-                    tag: loadedProduct.id,
-                    child: Image.network(
-                      'http://10.0.2.2:8000${loadedProduct.photo}',
-                      fit: BoxFit.cover,
+      child: Column(
+        children: [
+          SizedBox(
+            height: deviceSize.height * 0.55,
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: deviceSize.height * 0.45,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(40),
+                        bottomRight: Radius.circular(40)),
+                    child: Hero(
+                      tag: loadedProduct.id,
+                      child: Image.network(
+                        'http://10.0.2.2:8000${loadedProduct.photo}',
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ),
+                ListingDetailsCard(
+                    deviceSize: deviceSize, loadedProduct: loadedProduct)
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          ListingReviewCard(deviceSize: deviceSize),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            width: deviceSize.width * 0.9,
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              'Overview',
+              textAlign: TextAlign.start,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            width: deviceSize.width * 0.9,
+            child: Text(
+              loadedProduct.description,
+              style: const TextStyle(
+                  fontSize: 14, color: Color.fromRGBO(136, 136, 136, 1)),
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+}
+
+
+
+
+
+class ListingReviewCard extends StatelessWidget {
+  const ListingReviewCard({
+    super.key,
+    required this.deviceSize,
+  });
+
+  final Size deviceSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+            width: 1.4, color: const Color.fromRGBO(222, 234, 253, 1)),
+        color: const Color.fromRGBO(242, 247, 255, 1),
+      ),
+      height: 92,
+      width: deviceSize.width * 0.9,
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Reviews',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: kAccentColor),
               ),
-              Positioned(
-                  bottom: 1,
-                  child: Container(
-                    width: deviceSize.width,
-                    alignment: Alignment.center,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        border: Border.all(
-                            width: 1.4,
-                            color: const Color.fromRGBO(222, 234, 253, 1)),
-                        color: Color.fromRGBO(242, 247, 255, 1),
-                      ),
-                      height: 130,
-                      width: deviceSize.width * 0.9,
-                      child: Column(
-                        children: [
-                          Row(),
-                          Row(),
-                        ],
-                      ),
+              Row(
+                children: [
+                  const Icon(Icons.star,
+                      color: Color.fromRGBO(230, 194, 40, 1)),
+                  RichText(
+                      text: const TextSpan(
+                          style: TextStyle(
+                              color: kAccentColor,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                          text: '4.5',
+                          children: [
+                        TextSpan(
+                            text: "( 1290 Reviews )",
+                            style: TextStyle(
+                                color: kAccentColor, fontSize: 15))
+                      ]))
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ListingDetailsCard extends StatelessWidget {
+  const ListingDetailsCard({
+    super.key,
+    required this.deviceSize,
+    required this.loadedProduct,
+  });
+
+  final Size deviceSize;
+  final Listing loadedProduct;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 1,
+      child: Container(
+        width: deviceSize.width,
+        alignment: Alignment.center,
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+                width: 1.4, color: const Color.fromRGBO(222, 234, 253, 1)),
+            color: const Color.fromRGBO(242, 247, 255, 1),
+          ),
+          height: 130,
+          width: deviceSize.width * 0.9,
+          child: Column(
+            children: [
+              //Top row containing title, location and price per day
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      text: loadedProduct.title,
+                      style: const TextStyle(
+                          color: kAccentColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                      children: [
+                        TextSpan(
+                            style: const TextStyle(fontSize: 14),
+                            text:
+                                "\n${loadedProduct.city}, ${loadedProduct.country}")
+                      ],
                     ),
-                  ))
+                  ),
+                  RichText(
+                    textAlign: TextAlign.right,
+                    text: const TextSpan(
+                        text: '\$120',
+                        style: TextStyle(
+                            color: kAccentColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                        children: [
+                          TextSpan(
+                              text: '\n/Day',
+                              style:
+                                  TextStyle(color: kGradientTop, fontSize: 15))
+                        ]),
+                  )
+                ],
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //Bedroom Icon and Text
+                  Row(
+                    children: const [Icon(Icons.bed), Text('Bedroom')],
+                  ),
+                  //Bathroom Icon and Text
+                  Row(
+                    children: const [Icon(Icons.bathtub), Text('Bathroom')],
+                  ),
+                  //SQFT Icon and Text
+                  Row(
+                    children: const [Icon(Icons.square_foot), Text('sqft')],
+                  ),
+                ],
+              ),
             ],
           ),
-        )
-      ]),
-    ));
+        ),
+      ),
+    );
   }
 }
